e25628cf2e02935efcdaa5b13ad8560e65cd7833
Edgar Coronel <edgarcoronel66@gmail.com>
2024-11-05 11:29:19 -0700

    Se ajuste desfase para mostrar las citas de el día actual en adelante

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,30 @@
-import Appointment from '../models/Appointment.js'
-import { startOfDay } from 'date-fns'
+import Appointment from "../models/Appointment.js";
+import { startOfDay, subDays } from "date-fns";
 
 const getUserAppointments = async (req, res) => {
-    const { user } = req.params
+  const { user } = req.params;
 
-    if(user !== req.user._id.toString()) {
-        const error = new Error('Acceso Denegado')
-        return res.status(400).json({ msg: error.message })
-    }
+  if (user !== req.user._id.toString()) {
+    const error = new Error("Acceso Denegado");
+    return res.status(400).json({ msg: error.message });
+  }
 
-    try {
-        const today = startOfDay(new Date())
-        const query = req.user.admin ? {date: { $gte: today }} : {user: user, date: { $gte: today }}
-        const appointments = await Appointment
-                .find(query)
-                .populate('services')
-                .populate({path: 'user', select: 'company name lastName phone email'})
-                .sort({ date: 'asc'})
+  try {
+    // Cuando cambio el horario en estados unidos, dejo de traerse las citas de hoy, porlo que ahora indico que se traiga las citas de ayer para compensar ese desfase
+    // const today = startOfDay(new Date());
+    const today = startOfDay(subDays(new Date(), 1)); // Traigo las citas de ayer (hoy - 1) | En realidad se trae las de hoy en adelante.
+    const query = req.user.admin
+      ? { date: { $gte: today } }
+      : { user: user, date: { $gte: today } };
+    const appointments = await Appointment.find(query)
+      .populate("services")
+      .populate({ path: "user", select: "company name lastName phone email" })
+      .sort({ date: "asc" });
 
-        res.json(appointments)
+    res.json(appointments);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export { 
-    getUserAppointments 
-}
+export { getUserAppointments };

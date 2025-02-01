8e65637880d5f12091578b9685455877d5358d33
Edgar Coronel <edgarcoronel66@gmail.com>
2025-02-01 08:53:03 -0700

    Fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import Appointment from "../models/Appointment.js";
 import { startOfDay, subDays } from "date-fns";
+import mongoose from "mongoose";
 
 const getUserAppointments = async (req, res) => {
   const { user } = req.params;
@@ -10,16 +11,49 @@ const getUserAppointments = async (req, res) => {
   }
 
   try {
-    // Cuando cambio el horario en estados unidos, dejo de traerse las citas de hoy, porlo que ahora indico que se traiga las citas de ayer para compensar ese desfase
-    // const today = startOfDay(new Date());
-    const today = startOfDay(subDays(new Date(), 1)); // Traigo las citas de ayer (hoy - 1) | En realidad se trae las de hoy en adelante.
+    const today = startOfDay(subDays(new Date(), 1));
+
+    // Ajusta el query para convertir user a ObjectId si no es admin
     const query = req.user.admin
       ? { date: { $gte: today } }
-      : { user: user, date: { $gte: today } };
-    const appointments = await Appointment.find(query)
-      .populate("services")
-      .populate({ path: "user", select: "company name lastName phone email" })
-      .sort({ date: "asc" });
+      : { user: new mongoose.Types.ObjectId(user), date: { $gte: today } };
+
+    const appointments = await Appointment.aggregate([
+      { $match: query }, // Aplica el filtro
+      {
+        $addFields: {
+          timeNumeric: {
+            $add: [
+              {
+                $multiply: [
+                  { $toInt: { $arrayElemAt: [{ $split: ["$time", ":"] }, 0] } },
+                  60,
+                ],
+              }, // Convierte horas a minutos
+              { $toInt: { $arrayElemAt: [{ $split: ["$time", ":"] }, 1] } }, // Agrega los minutos
+            ],
+          },
+        },
+      },
+      { $sort: { date: 1, timeNumeric: 1 } },
+      {
+        $lookup: {
+          from: "services",
+          localField: "services",
+          foreignField: "_id",
+          as: "services",
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "user",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      { $unwind: "$user" },
+    ]);
 
     res.json(appointments);
   } catch (error) {
